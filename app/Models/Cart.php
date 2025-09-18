04c1e840af07d89d0ecd47e4c3ab51879ab99edc
gallanpw <gallan.widyanto@gmail.com>
2025-09-18 23:03:17 +0700

    Feat: Update eloquent relationships cart

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -14,4 +14,24 @@ class Cart extends Model
     protected $fillable = [
         'customer_id', 'product_id', 'qty'
     ];
+
+    /**
+     * product
+     *
+     * @return void
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * customer
+     *
+     * @return void
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
