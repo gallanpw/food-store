b89c6d084bf8ddb47a2a14707c94da861e7e0f68
gallanpw <gallan.widyanto@gmail.com>
2025-09-18 22:57:22 +0700

    Feat: Update eloquent relationships transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -24,4 +24,64 @@ class Transaction extends Model
         'status',
         'snap_token',
     ];
+
+    /**
+     * TransactionDetails
+     *
+     * @return void
+     */
+    public function TransactionDetails()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+
+    /**
+     * shipping
+     *
+     * @return void
+     */
+    public function shipping()
+    {
+        return $this->hasOne(Shipping::class);
+    }
+
+    /**
+     * customer
+     *
+     * @return void
+     */
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    /**
+     * province
+     *
+     * @return void
+     */
+    public function province()
+    {
+        return $this->belongsTo(Province::class);
+    }
+
+    /**
+     * city
+     *
+     * @return void
+     */
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    /**
+     * district
+     *
+     * @return void
+     */
+    public function district()
+    {
+        return $this->belongsTo(District::class);
+    }
 }
